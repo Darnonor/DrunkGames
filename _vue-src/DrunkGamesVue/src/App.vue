2a22f4943c5f82af2f978519bc1dd159c66c86bc
Igor.Krosh <igor.krosh@gmail.com>
2019-06-02 03:15:44 +0300

    Первые наброски компоненты главного меню

--- _vue-src/DrunkGamesVue/src/App.vue
+++ _vue-src/DrunkGamesVue/src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <MainPage/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainPage
   }
 }
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css?family=Lobster&display=swap&subset=cyrillic');
+@import url('https://fonts.googleapis.com/css?family=El+Messiri:700&display=swap&subset=cyrillic');
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body
+{
+  height: 100vh;
+  overflow: hidden;
+  /*background: linear-gradient(#f5af19, #f12711)*/
+  /*background: linear-gradient(#DA4453, #89216B);*/
+  background: linear-gradient(#ff6a00, #ee0979)
 }
 </style>
---
+++ _vue-src/DrunkGamesVue/src/components/MainPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="logo">
+            <span>
+                АлкоИгрища
+            </span>
+        </div>
+        <b-form-input placeholder="Игрок 1"></b-form-input>
+        <b-form-input placeholder="Игрок 2"></b-form-input>
+        <b-form-input placeholder="Игрок 3"></b-form-input>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainPage',
+    props: {
+        //msg: String
+    }
+}
+</script>
+
+<style>
+.logo
+{
+    font-family: 'Lobster', cursive;
+    /*font-family: 'El Messiri', sans-serif;*/
+    font-size: 52px;
+    color: white;
+    margin-top: 35px;
+    margin-bottom: 35px;
+}
+
+.container
+{
+    padding: 15px;
+    text-align: center;
+}
+
+.form-control
+{
+    margin-bottom: 15px;
+    border-radius: 0;
+    border: 0;
+    background-color:initial;
+    color:white;
+    margin-left: auto;
+    margin-right: auto;
+    width:70%;
+}
+</style>
